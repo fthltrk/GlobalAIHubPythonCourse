f3bdaaaf290b66a74dc8329ed66ce30d63fdee44
fthltrk <fthltrk@users.noreply.github.com>
2021-02-21 20:54:37 +0300

    ödev3

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,27 @@
-#Explain your work
+primeFirst=[]
+primeSecond=[]
+def prime_first(first):
+    primeFirst.append(first)
+    if first==499:
+        print("Until 500: ",primeFirst)
 
-#Question 1
-for x in range(a):
-	print(a)
+def prime_second(second):
+    primeSecond.append(second)
+    if second==997:
+        print("After 500: ",primeSecond)
+
+
+for i in range(2,1000):
+    prime=True
+    if i<500:
+        for j in range(2,i):
+            if i%j==0:
+                prime=False
+        if prime:
+            prime_first(i)
+    else:
+        for j in range(2,i):
+            if i%j==0:
+                prime=False
+        if prime:
+            prime_second(i)
