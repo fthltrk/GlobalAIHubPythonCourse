02b9d1e02091cd6e1cee81895b15a5892447c348
fthltrk <fthltrk@users.noreply.github.com>
2021-02-21 15:37:51 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,21 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+import random
+matrix= [[1,1,1],[1,1,1],[1,1,1]] 
+for i in range(3):
+    for j in range(3):
+        while True:
+            x=0
+            randomnumber=random.randint(2,100)
+            if randomnumber==2:
+                matrix[i][j]=randomnumber
+                break
+            else:
+                for k in range (2, randomnumber):
+                    if randomnumber%k==0:
+                        x=1
+                        continue
+                if x==1:
+                    continue
+                else:   
+                    matrix[i][j]=randomnumber
+                    break        
+print(matrix)
